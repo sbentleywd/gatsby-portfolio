239f2380dac6cb0e5334797af151d130d4af45ec
sbentley85 <61815607+sbentley85@users.noreply.github.com>
2020-09-09 12:04:22 +1200

    remove homepage link

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => {
       <h1>Hello</h1>
       <h2>I'm Simon, a full-stack developer</h2>
       
-      <p>Need a developer? <Link to="/contact">Contact me</Link></p>
+      
       
 
     </Layout>
